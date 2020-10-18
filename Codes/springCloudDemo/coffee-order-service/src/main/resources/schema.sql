93839f4c1c7b9b8ea5872ea6028ae0ee9b4edb6e
IamDingj <iamdingj@163.com>
2020-10-18 23:38:32 +0800

    register update

--- Codes/springCloudDemo/coffee-order-service/src/main/resources/schema.sql
+++ Codes/springCloudDemo/coffee-order-service/src/main/resources/schema.sql
@@ -9,3 +9,29 @@ create table t_user (
     update_time timestamp,
     primary key (id)
 );
+
+drop table t_coffee if exists;
+create table t_coffee (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    name varchar(255),
+    price bigint,
+    primary key (id)
+);
+
+drop table t_order if exists;
+create table t_order (
+    id bigint auto_increment,
+    create_time timestamp,
+    update_time timestamp,
+    customer varchar(255),
+    state integer not null,
+    primary key (id)
+);
+
+drop table t_order_coffee if exists;
+create table t_order_coffee (
+    coffee_order_id bigint not null,
+    items_id bigint not null
+);
