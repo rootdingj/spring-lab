5683b8623a5e2bf2dc998f338b2ed3367d08462e
IamDingj <iamdingj@163.com>
2020-10-28 23:41:34 +0800

    update

---
+++ Codes/springDemo/zk-demo/src/main/java/com/dj/zk/WatcherDemo.java
@@ -0,0 +1,17 @@
+package com.dj.zk;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+/**
+ * @Auther: steven
+ * @Date: 2020/08/28
+ * @Description: watcher 事件机制
+ */
+public class WatcherDemo implements Watcher {
+
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
+}
